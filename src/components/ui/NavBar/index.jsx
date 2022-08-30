18cbef7fbbc452e3f5f31a0cd3f0a639a10f9592
kiran-ssr <dev.kssr@gmail.com>
2022-08-30 14:12:12 -0400

    NavBar dev-Completed

--- src/components/ui/NavBar/index.jsx
+++ src/components/ui/NavBar/index.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import "./navbar.css";
+import { useMediaQuery } from "react-responsive";
 import {
   AiOutlineHome,
   AiOutlineUser,
   AiOutlineAppstore,
+  AiOutlineCloseCircle,
+  AiOutlineMessage,
+  AiOutlineCode,
 } from "react-icons/ai";
 import { FiAward } from "react-icons/fi";
-import { CgCloseR } from "react-icons/cg";
-import { FaCode } from "react-icons/fa";
-import { GrContact, GrAchievement } from "react-icons/gr";
+import { BiCodeAlt } from "react-icons/bi";
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
+  const iconBreakPoint = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
     <header className="header">
@@ -24,50 +27,56 @@ const NavBar = () => {
           <ul className="nav_list grid">
             <li className="nav_item">
               <a href="#home" className="nav_link active-link">
-                <AiOutlineHome fontSize={"20px"} />
+                {iconBreakPoint && <AiOutlineHome fontSize={"18px"} />}
                 Home
               </a>
             </li>
 
             <li className="nav_item">
               <a href="#about" className="nav_link">
-                <AiOutlineUser fontSize={"20px"} />
+                {iconBreakPoint && <AiOutlineUser fontSize={"18px"} />}
                 About
               </a>
             </li>
 
             <li className="nav_item">
               <a href="#qualification" className="nav_link">
-                <GrAchievement fontSize={"20px"} />
+                {iconBreakPoint && <FiAward fontSize={"18px"} />}
                 Qualification
               </a>
             </li>
 
             <li className="nav_item">
               <a href="#skills" className="nav_link">
-                <FiAward fontSize={"20px"} />
+                {iconBreakPoint && <BiCodeAlt fontSize={"18px"} />}
                 Skills
               </a>
             </li>
             <li className="nav_item">
               <a href="#projects" className="nav_link">
-                <FaCode fontSize={"20px"} />
+                {iconBreakPoint && <AiOutlineCode fontSize={"18px"} />}
                 Projects
               </a>
             </li>
 
             <li className="nav_item">
               <a href="#contact" className="nav_link">
-                <GrContact fontSize={"20px"} />
+                {iconBreakPoint && <AiOutlineMessage fontSize={"18px"} />}
                 Contact
               </a>
             </li>
           </ul>
+
+          <AiOutlineCloseCircle
+            className="nav-toggle nav_close"
+            onClick={() => setToggle(!toggle)}
+          />
         </div>
 
-        <div className="nav_toggle" onClick={() => setToggle(!toggle)}>
-          {toggle ? <CgCloseR /> : <AiOutlineAppstore />}
-        </div>
+        <AiOutlineAppstore
+          className="nav_toggle"
+          onClick={() => setToggle(!toggle)}
+        />
       </nav>
     </header>
   );
